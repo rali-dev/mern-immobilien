b460526655f638729b57fbfe5bc51f4471436b8f
rahimi.ali <ali.saee@outlook.de>
2026-01-02 15:33:52 +0100

    4. create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,22 +12,21 @@ export default function Header() {
             <span className='text-slate-700'>Immobilien</span>
           </h1>
         </Link>
-          <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
             <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
             <FaSearch className='text-slate-600' />
-          </form>
-          <ul className='flex justify-between gap-4 '>
+        </form>
+        <ul className='flex justify-between gap-4 '>
             <Link to='/'>
             <li className='hidden sm:inline hover:underline'>Home</li>
             </Link>
             <Link to='/about'>
                 <li className='hidden sm:inline hover:underline'>About</li>
             </Link>
-
             <Link to='/sign-in'>
                 <li className='sm:inline hover:underline'>Sign in</li>
             </Link>
-          </ul>
+        </ul>
       </div>    
     </header>
   )
